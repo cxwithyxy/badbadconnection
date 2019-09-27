df5e0d5379ecd1ddfe6f381ecafbf78c6e03cef2
chenxuan <523764720@qq.com>
2019-09-27 18:36:52 +0800

    单元测试发现, 客户端会收到自己发送的包

--- test/Badbadconnection.ts
+++ test/Badbadconnection.ts
@@ -27,71 +27,71 @@ describe("Badbadconnection", function ()
 
     describe("基础通讯", async () =>
     {
-        // it("发信息并收信息", async () =>
-        // {
-        //     let recv_msg = ""
-        //     await new Promise((succ) =>
-        //     {
-        //         b1.on_recv((msg: string) =>
-        //         {
-        //             recv_msg = msg
-        //             succ()
-        //         })
-        //         b2.send(test_msg)
-        //     })
-        //     should(recv_msg).equal(test_msg)
-        // })
+        it("发信息并收信息", async () =>
+        {
+            let recv_msg = ""
+            await new Promise((succ) =>
+            {
+                b1.on_recv((msg: string) =>
+                {
+                    recv_msg = msg
+                    succ()
+                })
+                b2.send(test_msg)
+            })
+            should(recv_msg).equal(test_msg)
+        })
 
-        // it("每次只发一条信息", async () =>
-        // {
-        //     let recv_str_list: string[] = []
-        //     let send_time_loop = 5
-        //     b1.on_recv((msg: string) =>
-        //     {
-        //         recv_str_list.push(msg)
-        //     })
-        //     for(let i  = 0; i < 5; i++)
-        //     {
-        //         await b2.send(`${test_msg}${i}`)
-        //     }
-        //     await sleep(5 * 1e3)
-        //     should(recv_str_list.length).equal(send_time_loop)
-        //     for(let i  = 0; i < 5; i++)
-        //     {
-        //         should(recv_str_list[i]).equal(`${test_msg}${i}`)
-        //     }
-        // })
+        it("每次只发一条信息", async () =>
+        {
+            let recv_str_list: string[] = []
+            let send_time_loop = 5
+            b1.on_recv((msg: string) =>
+            {
+                recv_str_list.push(msg)
+            })
+            for(let i  = 0; i < 5; i++)
+            {
+                await b2.send(`${test_msg}${i}`)
+            }
+            await sleep(5 * 1e3)
+            should(recv_str_list.length).equal(send_time_loop)
+            for(let i  = 0; i < 5; i++)
+            {
+                should(recv_str_list[i]).equal(`${test_msg}${i}`)
+            }
+        })
 
-        // it("不会受到自己发送的信息", async () =>
-        // {
-        //     let b1_send = `b1_send${Math.random()}`
-        //     let b1_recv: string = ""
-        //     let b2_recv: string = ""
-        //     let b2_promise = new Promise((succ) =>
-        //     {
-        //         b2.on_recv((msg: string) =>
-        //         {
-        //             b2_recv = msg
-        //             succ()
-        //         })
-        //     })
-        //     let b1_promise = new Promise((succ) =>
-        //     {
-        //         b1.on_recv((msg: string) =>
-        //         {
-        //             b1_recv = msg
-        //             succ()
-        //         })
-        //         setTimeout(() =>
-        //         {
-        //             succ()
-        //         }, 5e3)
-        //     })
-        //     await b1.send(b1_send)
-        //     await Promise.all([b1_promise, b2_promise])
-        //     should(b1_recv).not.equal(b1_send)
-        //     should(b2_recv).equal(b1_send)
-        // })
+        it("不会受到自己发送的信息", async () =>
+        {
+            let b1_send = `b1_send${Math.random()}`
+            let b1_recv: string = ""
+            let b2_recv: string = ""
+            let b2_promise = new Promise((succ) =>
+            {
+                b2.on_recv((msg: string) =>
+                {
+                    b2_recv = msg
+                    succ()
+                })
+            })
+            let b1_promise = new Promise((succ) =>
+            {
+                b1.on_recv((msg: string) =>
+                {
+                    b1_recv = msg
+                    succ()
+                })
+                setTimeout(() =>
+                {
+                    succ()
+                }, 5e3)
+            })
+            await b1.send(b1_send)
+            await Promise.all([b1_promise, b2_promise])
+            should(b1_recv).not.equal(b1_send)
+            should(b2_recv).equal(b1_send)
+        })
 
         function get_random_big_msg(max_msg_length: number)
         {
@@ -122,5 +122,4 @@ describe("Badbadconnection", function ()
             })
         })
     })
-
 })
